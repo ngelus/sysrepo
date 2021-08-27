2940c7097e5f270f217238ff181232c27b804f63
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-08-27 15:56:30 +0200

    Update agdlp.ps1

--- posh/agdlp.ps1
+++ posh/agdlp.ps1
@@ -27,7 +27,7 @@ write-host "Erstelle gerade ${groupnumber} globale Gruppen..."
   $globalgroupname = New-ADGroup "${globalgroupinput}" -path "OU=Globale Gruppen, DC=${domainname}, dc=${domainprefix}" -GroupScope Global -GroupCategory Security 
 
   # füge noch benutzer hinzu
-  write-host "Geben Sie einen Gruppenmitglied an (username) "
+  $username = write-host "Geben Sie einen Gruppenmitglied an (username) "
   Add-ADGroupMember -Identity ${globalgroupname} -Member ${username}
 }
 
