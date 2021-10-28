d090333865472dd187323e8cb079ca8c5dc9a803
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-10-28 12:45:05 +0200

    Create Dialer.ps1

---
+++ CUCM/API/Dialer.ps1
@@ -0,0 +1,18 @@
+function CiscoDialApi {
+  param (
+  )
+  [string] $input = read-host -prompt "Geben Sie bitte die IP Ihres Cisco Tel ein "
+  [string] $uri = "http://${input}/CGI/Execute"
+  [string] $target = read-host -prompt "Geben Sie bitte die Nummer an, die Sie anrufen wollen "
+  
+  [string] $xmlbody = @"
+  <CiscoIPPhoneExecute>
+    <ExecuteItem URL="Dial:${target}" />
+  </CiscoIPPhoneExecute>
+  "@
+  
+  write-host "Rufe die Nummer: ${target} an..."
+  Invoke-WebRequest "${uri}" -Method Post -ContentType "text/xml" -Body $xmlbody
+}
+
+CiscoDialApi
