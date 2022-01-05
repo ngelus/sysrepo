baf3893bf3542ac204bd780497ce429e024bcda1
Ngelus <25223259+ngelus@users.noreply.github.com>
2022-01-05 11:30:24 +0100

    Turned this into an advanced function

--- posh/Funcs.ps1
+++ posh/Funcs.ps1
@@ -1,5 +1,25 @@
 function myfunc {
+
+  <#
+     
+        .SYNOPSIS
+        [Insert a short description of what the function is supposed to do]
+        
+        .DESCRIPTION
+        [Insert a longer description of what the function is supposed to do]
+        
+        .PARAMETER myint
+        [Insert a short description of this parameter]
+      
+        .EXAMPLE
+        myfunc 1
+        myfunc -myint 1
+      
+  #>
+
+  [CmdletBinding()]
   param (
+    [Parameter(Mandatory=$false)]
     [int] $myint
   )
 
